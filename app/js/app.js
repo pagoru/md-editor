dd857ca706c607805a9caa3b057479950c162d39
Damian Baćkowski <damianbackowski@gmail.com>
2016-04-20 13:24:17 +0200

    remove jQuery

--- app/js/app.js
+++ app/js/app.js
@@ -1,62 +1,60 @@
-$(document).ready(function() {
-  var languageOverrides = {
-    js: 'javascript',
-    html: 'xml'
-  };
+var languageOverrides = {
+  js: 'javascript',
+  html: 'xml'
+};
 
-  var editor = CodeMirror.fromTextArea(document.getElementById('input'), {
-    mode: 'gfm',
-    lineNumbers: false,
-    matchBrackets: true,
-    lineWrapping: true,
-    theme: 'monokai',
-    extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"}
-  });
+var editor = CodeMirror.fromTextArea(document.getElementById('input'), {
+  mode: 'gfm',
+  lineNumbers: false,
+  matchBrackets: true,
+  lineWrapping: true,
+  theme: 'monokai',
+  extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"}
+});
 
-  var md = markdownit({
-    html: true,
-    linkify: true,
-    highlight: function(code, lang){
-      if(languageOverrides[lang]) lang = languageOverrides[lang];
-      if(lang && hljs.getLanguage(lang)){
-        try {
-          return hljs.highlight(lang, code).value;
-        }catch(e){
-        }
+var md = markdownit({
+  html: true,
+  linkify: true,
+  highlight: function(code, lang){
+    if(languageOverrides[lang]) lang = languageOverrides[lang];
+    if(lang && hljs.getLanguage(lang)){
+      try {
+        return hljs.highlight(lang, code).value;
+      }catch(e){
       }
-      return '';
     }
-  })
-
-  function update(e) {
-    markdownRender(e.getValue());
+    return '';
   }
+})
 
-  function markdownRender(input) {
-    var out = document.getElementById('right');
-    var old = out.cloneNode(true);
-    out.innerHTML = md.render(input);
-  }
+function update(e) {
+  markdownRender(e.getValue());
+}
 
-  editor.on('change', update);
+function markdownRender(input) {
+  var out = document.getElementById('right');
+  var old = out.cloneNode(true);
+  out.innerHTML = md.render(input);
+}
 
-  markdownRender(document.getElementById('input').value);
+editor.on('change', update);
 
-  var electron = require('electron');
+markdownRender(document.getElementById('input').value);
 
-  electron.ipcRenderer.on('new-file', function(event, message) {
-    editor.setValue('');
-  });
+var electron = require('electron');
 
-  electron.ipcRenderer.on('load-file', function(event, message) {
-    editor.setValue(message);
-  });
+electron.ipcRenderer.on('new-file', function(event, message) {
+  editor.setValue('');
+});
 
-  electron.ipcRenderer.on('get-editor-content', function(event, message) {
-    electron.ipcRenderer.send('editor-content', editor.getValue());
-  });
+electron.ipcRenderer.on('load-file', function(event, message) {
+  editor.setValue(message);
+});
 
-  electron.ipcRenderer.on('get-output-content', function(event, message) {
-    electron.ipcRenderer.send('output-content', document.getElementById('right').innerHTML);
-  });
+electron.ipcRenderer.on('get-editor-content', function(event, message) {
+  electron.ipcRenderer.send('editor-content', editor.getValue());
+});
+
+electron.ipcRenderer.on('get-output-content', function(event, message) {
+  electron.ipcRenderer.send('output-content', document.getElementById('right').innerHTML);
 });
